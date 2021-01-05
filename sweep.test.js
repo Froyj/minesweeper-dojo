d53e7d27683dc1dfadab27f31c6ccf0702dd8a38
Geoffroy <geoffroy.periot.dev@gmail.com>
2021-01-05 11:12:52 +0100

    enhanced tests

--- sweep.test.js
+++ sweep.test.js
@@ -1,90 +1,104 @@
-const sweep = require('./sweep');
+const sweep = require("./sweep");
 
+describe("sweep", () => {
+  test("is a function", () => {
+    expect(typeof sweep).toBe("function");
+  });
 
-test('sweep is a function', () => {
-  expect(typeof sweep).toBe('function');
-});
-
-test('sweep has 3 parameters', () => {
-  expect(sweep.length).toBe(3);
-});
-
-const grid = [
-  [0, 0, 0, 1],
-  [0, 1, 0, 0],
-  [1, 0, 0, 0],
-];
-
-test('sweep returns kaboom if coordinates point to bomb', () => {
-  expect(sweep(grid, 1, 1)).toBe("kaboom");
+  test("has 3 parameters", () => {
+    expect(sweep.length).toBe(3);
+  });
 });
 
-test('sweep returns kaboom if coordinates point to bomb', () => {
-  expect(sweep(grid, 0, 3)).toBe("kaboom");
+describe("when bomb on tested coordinates, sweep returns", () => {
+  const grid = [
+    [0, 0, 0, 1],
+    [0, 1, 0, 0],
+    [1, 0, 0, 0],
+  ];
+
+  test("kaboom", () => {
+    expect(sweep(grid, 1, 1)).toBe("kaboom");
+  });
+
+  test("kaboom", () => {
+    expect(sweep(grid, 0, 3)).toBe("kaboom");
+  });
+
+  test("kaboom", () => {
+    expect(sweep(grid, 2, 0)).toBe("kaboom");
+  });
 });
 
-test('sweep returns kaboom if coordinates point to bomb', () => {
-  expect(sweep(grid, 2, 0)).toBe("kaboom");
+describe("when no bomb on tested coordinates, sweep returns", () => {
+  test("1 as the number of adjacent bombs", () => {
+    expect(sweep(grid, 0, 0)).toBe(1);
+  });
+
+  test("2 as the number of adjacent bombs", () => {
+    expect(sweep(grid, 1, 2)).toBe(2);
+  });
+
+  test("0 when no adjacent bombs", () => {
+    expect(sweep(grid, 2, 3)).toBe(0);
+  });
 });
 
-test('sweep return 1 as the number of adjacent bombs', () => {
-  expect(sweep(grid, 0, 0)).toBe(1);
+describe("", () => {
+  test("null grid throw a TypeError", () => {
+    expect(() => sweep(null, 1, 1)).toThrowError(TypeError);
+  });
+  test("wrong grid type value throw a TypeError", () => {
+    expect(() => sweep("a", 1, 1)).toThrowError(TypeError);
+  });
+  test("wrong type value in grid throw a TypeError", () => {
+    expect(() =>
+      sweep(
+        [
+          [0, 0, "0", 1],
+          [0, 1, 0, 1],
+          [1, 0, 0, 0],
+        ],
+        1,
+        1
+      )
+    ).toThrowError(TypeError);
+  });
+  test("negative value in grid throw a TypeError", () => {
+    expect(() =>
+      sweep(
+        [
+          [0, 0, 0, 1],
+          [0, -1, 0, 1],
+          [1, 0, 0, 0],
+        ],
+        1,
+        1
+      )
+    ).toThrowError(TypeError);
+  });
+  test("null value in grid throw a TypeError", () => {
+    expect(() =>
+      sweep(
+        [
+          [0, 0, 0, 1],
+          [0, 1, null, 1],
+          [1, 0, 0, 0],
+        ],
+        0,
+        1
+      )
+    ).toThrowError(TypeError);
+  });
 });
 
-test('sweep return 2 as the number of adjacent bombs', () => {
-  expect(sweep(grid, 1, 2)).toBe(2);
+describe("", () => {
+  test("invalid row value grid throw a RangeError", () => {
+    expect(() => sweep(grid, -1, 1)).toThrowError(RangeError);
+  });
+  test("invalid column value throw a RangeError", () => {
+    expect(() => sweep(grid, 1, 5)).toThrowError(RangeError);
+  });
 });
-
-test('sweep return 0 when no adjacent bombs', () => {
-  expect(sweep(grid, 2, 3)).toBe(0);
-});
-
-test('null grid throw a TypeError', () => {
-  expect(sweep(null, 1, 1)).toThrowError(TypeError);
-});
-test('wrong grid type value throw a TypeError', () => {
-  expect(sweep("a", 1, 1)).toThrowError(TypeError);
-});
-test('wrong type value in grid throw a TypeError', () => {
-  expect(sweep(
-    [
-      [0, 0, "0", 1],
-      [0, 1, 0, 1],
-      [1, 0, 0, 0],
-    ],
-    1,
-    1
-  )).toThrowError(TypeError);
-});
-test('negative value in grid throw a TypeError', () => {
-  expect(sweep(
-    [
-      [0, 0, 0, 1],
-      [0, -1, 0, 1],
-      [1, 0, 0, 0],
-    ],
-    1,
-    1
-  )).toThrowError(TypeError);
-});
-test('null value in grid throw a TypeError', () => {
-  expect(sweep(
-    [
-      [0, 0, 0, 1],
-      [0, 1, null, 1],
-      [1, 0, 0, 0],
-    ],
-    0,
-    1
-  )).toThrowError(TypeError);
-});
-
-test('invalid row value throw a RangeError', () => {
-  expect(sweep(grid, -1, 1)).toThrowError(RangeError);
-});
-test('invalid column value throw a RangeError', () => {
-  expect(sweep(grid, 1, 5)).toThrowError(RangeError);
-});
-
 
 // End of tests
